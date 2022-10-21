fe115c1128331bb8516d10f925da5093a0a5c4ee
Qkite <yeonjigu4@gmail.com>
2022-10-21 14:38:10 +0900

    add에 익명 클래스 적용하기

--- src/main/java/dblinking/dao/UserDao.java
+++ src/main/java/dblinking/dao/UserDao.java
@@ -57,7 +57,22 @@ public class UserDao {
 
 
     public void add(User user) throws SQLException, ClassNotFoundException {
-        jdbcContextWithStatementStrategy(new AddStrategy(user));
+//        jdbcContextWithStatementStrategy(new AddStrategy(user));
+        jdbcContextWithStatementStrategy(
+                // 익명 클래스 적용
+                new StatementStrategy() {
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values (?, ?, ?)");
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
+
+                        return ps;
+                    }
+                }
+        );
+
 
     }
 
